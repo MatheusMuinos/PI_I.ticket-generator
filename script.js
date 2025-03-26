937538c4387d538077480e9edced2f0430c44360
Matheus Sarno Muinos <matheusmuinos@gmail.com>
2025-03-26 17:30:44 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function showMessage() {
-    alert('Olá do JavaScript!');
-}
+document.getElementById('ticketForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var event = document.getElementById('event').value;
+    var date = new Date(document.getElementById('date').value).toLocaleDateString();
+
+    document.getElementById('personName').textContent = name;
+    document.getElementById('eventName').textContent = event;
+    document.getElementById('eventDate').textContent = date;
+});
